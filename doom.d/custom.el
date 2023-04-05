cd1d008c1257d2ec05a59c08726447323df48993
jpenno <jpenno123@hotmail.com>
2023-04-05 19:21:17 +1000

    add doom config

---
+++ doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e09401ab2c457e2e4d8b800e1c546dbc8339dc33b2877836ba5d9b6294ae6e55" default))
+ '(package-selected-packages
+   '(vscode-dark-plus-theme vscdark-theme dakrone-theme ef-themes mood-one-theme github-theme cyberpunk-theme ample-theme uwu-theme nyx-theme ayu-theme abyss-theme one-themes moe-theme color-theme-sanityinc-tomorrow melancholy-theme vs-dark-theme atom-one-dark-theme org-roam)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
