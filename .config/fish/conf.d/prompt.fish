761db8cad85b4b7994e6032245546efa80e78932
jpenn <jpenno123@hotmail.com>
2024-06-12 17:01:54 +1000

    updade fish config

---
+++ .config/fish/conf.d/prompt.fish
@@ -0,0 +1,9 @@
+#
+# Prompt
+#
+
+# Disable new user greeting.
+set fish_greeting 
+
+# Initialize posh
+oh-my-posh init fish --config ~/.config/ohmyposh/zen.toml | source
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,1 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-  fish_add_path ~/bin/
-  oh-my-posh init fish --config ~/.config/ohmyposh/zen.toml | source
-end
+# ~/.config/fish/conf.d runs first
